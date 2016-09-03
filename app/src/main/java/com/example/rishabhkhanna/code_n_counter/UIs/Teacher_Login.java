747b9d50bda297d8f52c66d2c7b77e049b962949
Devesh Saini <deveshsaini96@gmail.com>
2016-09-03 23:06:33 +0530

    Logins

---
+++ app/src/main/java/com/example/rishabhkhanna/code_n_counter/UIs/Teacher_Login.java
@@ -0,0 +1,15 @@
+package com.example.rishabhkhanna.code_n_counter.UIs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.rishabhkhanna.code_n_counter.R;
+
+public class Teacher_Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher__login);
+    }
+}
