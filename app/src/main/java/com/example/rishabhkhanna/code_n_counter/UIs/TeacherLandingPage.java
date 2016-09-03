dbaca067128b12f0637123ddda587f27301c3f7c
Devesh Saini <deveshsaini96@gmail.com>
2016-09-03 23:02:12 +0530

    Drawer_FIX

--- app/src/main/java/com/example/rishabhkhanna/code_n_counter/UIs/TeacherLandingPage.java
+++ app/src/main/java/com/example/rishabhkhanna/code_n_counter/UIs/TeacherLandingPage.java
@@ -74,14 +74,13 @@ public class TeacherLandingPage extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, "Open navigation drawer", "Close navigation drawer");
+        final DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(TeacherLandingPage.this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_drawer);
+
 
 
 
