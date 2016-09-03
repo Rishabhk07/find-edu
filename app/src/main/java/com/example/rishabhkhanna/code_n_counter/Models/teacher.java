625dd545f43e19464fd5a01ef38d0270dfed2ad5
Rishabh Khanna <rishabhkhanna777@gmail.com>
2016-09-03 14:38:56 +0530

    Teacher signup added

--- app/src/main/java/com/example/rishabhkhanna/code_n_counter/Models/teacher.java
+++ app/src/main/java/com/example/rishabhkhanna/code_n_counter/Models/teacher.java
@@ -4,29 +4,61 @@ package com.example.rishabhkhanna.code_n_counter.Models;
  * Created by rishabhkhanna on 03/09/16.
  */
 public class teacher {
+
+    String teacherId;
     String username;
     String email;
     String password;
+    String qualification;
+    String workingAt;
+    String age;
+    String contact;
 
 
     public teacher() {
     }
 
-    public teacher(String username, String email, String password) {
+
+    public teacher(String username, String email, String password, String qualification, String workingAt, String age, String contact, String teacherId) {
         this.username = username;
         this.email = email;
         this.password = password;
+        this.qualification = qualification;
+        this.workingAt = workingAt;
+        this.age = age;
+        this.contact = contact;
+        this.teacherId = teacherId;
+    }
+
+    public String getTeacherId() {
+        return teacherId;
     }
 
     public String getUsername() {
         return username;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
     public String getPassword() {
         return password;
     }
 
-    public String getEmail() {
-        return email;
+    public String getQualification() {
+        return qualification;
+    }
+
+    public String getWorkingAt() {
+        return workingAt;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getContact() {
+        return contact;
     }
 }
